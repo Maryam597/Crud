d300f60e967e41b70162e3ea0be0ef9ce5212ffa
Maryam597 <maryam.dri@hotmail.fr>
2023-07-25 15:56:39 +0200

    HTML CSS

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+})
+
+
+
+// Register a new user 
+
+const createUser = (req, res) => {
+    // Utilise req.body de body.parser
+   const { lastname, firstname, address, city, zipcode, email, phonenumber } = req.body;
+   // Vérifier si les champs sont remplis
+
+   if (!email || !lastname || !firstname || !address || !city || !zipcode || !phonenumber ) {
+     return res.status(400).json({
+        error: 'Données manquantes '
+     }) 
+   }
+
+   const query = 'INSERT INTO user (email, lastname, firstname, address, city, zipcode, phonenumber) VALUES (?, ?, ?, ?, ?, ?, ?)';
+   conn.query(query, [lastname, firstname, address, city, zipcode, email, phonenumber], (err) => {
+
+    if(err) {
+        console.error('Erreur lors de l\'insertion d\'un utilisateur :' + err);
+        res.status(500).json({ error: 'Erreur lors de l\'insertion des données' });
+    } else {
+        res.status(200).json({ message: 'Utilisateur enregistré'});
+    }
+});
+};
+
+// Get all users
+const getAllUsers = (req, res) => {
+    const query = 'SELECT * FROM user'; 
+    conn.query(query, (err, result) => {
+        if(err) {
+            console.error('Erreur lors de la récupération des données :' + err);
+            res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+        }
+        else {
+            res.status(200).json(result);
+        }
+
+    })
+}
+
+module.exports = {
+    createUser,
+    getAllUsers,
+};
---
+++ server/server.js
@@ -0,0 +1,50 @@
+// importation des dépendances express, body parser et cors 
+require('dotenv').config();
+const express = require('express');   
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user.route');
+const cors = require('cors');
+
+// Connexion à la base de données 
+const connectDB = require('./config/db');
+
+// Middlewares
+const app = express(); // app est égale à express, tout ce qu'il y a dans la config express
+
+// use app Express
+app.use(express.json()); // app.use --> utilisation
+
+// use bodyParse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // urlencoded : pr laisser l'url tel qu'il est 
+
+//use cors
+app.use(
+    cors({
+    origin: 'http://localhost:5500', // url du client --> donne l'autorisation d'accès au client 
+    optionSuccessStatus: 200, 
+})
+);
+
+//Routes 
+app.use('/', userRoutes);
+
+// Ecrire bonjour dans le navigateur
+app.get('/', (req, res) => {
+    res.send('Hello World of Boredom')
+})
+
+// Configuration et lancement du serveur 
+const start = async () => {   // fonction fléchée 
+    try {
+        await connectDB();
+        const port = process.env.PORT || 6000; // port de base OU bien port de secours 
+        app.listen(port, () => {
+            console.log(`Le serveur a démarré sur le port ${port}`);
+        })
+    } catch {
+        console.log(`Erreur lors du démarrage du serveur`);
+    }
+}; 
+
+start(); 
